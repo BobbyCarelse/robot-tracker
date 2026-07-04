341ab463dfb83942f1949262b8cb23334ce3f272
Bobby Carelse <bobby@juta.co.za>
2026-07-04 16:28:16 +0200

    feat: create edge utility to store lost points

---
+++ src/utils/EdgeUtil.ts
@@ -0,0 +1,24 @@
+const EDGE_STORAGE_KEY = "EDGE_OF_WORLD";
+
+export const getEdgePointsStorage = () => {
+  let store = localStorage.getItem(EDGE_STORAGE_KEY);
+
+  let arrayOfData: { x: number; y: number }[] = [];
+
+  if (store === null) {
+    arrayOfData = [];
+  } else {
+    arrayOfData = JSON.parse(store) as { x: number; y: number }[];
+  }
+
+  return arrayOfData;
+};
+
+export const storeEdgePoint = (x: number, y: number) => {
+  const storage = getEdgePointsStorage();
+
+  localStorage.setItem(
+    EDGE_STORAGE_KEY,
+    JSON.stringify([...storage, { x, y }]),
+  );
+};
