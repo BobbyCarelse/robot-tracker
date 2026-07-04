0208f102cd7c308b91d171c12cdaea8f5f206c0f
Bobby Carelse <bobby@juta.co.za>
2026-07-04 16:27:33 +0200

    chore: create validation to check object values

---
+++ src/section/InstructionSection/validation.ts
@@ -0,0 +1,18 @@
+import * as yup from "yup";
+import { cardinalDirections } from "../../constants/app";
+
+export const validationSchema = yup.object().shape({
+  x: yup
+    .number()
+    .max(50, "The maximum x coordinate value is 50")
+    .required("Please provide the starting X Coordinate"),
+  y: yup
+    .number()
+    .max(50, "The maximum y coordinate value is 50")
+    .required("Please provide the starting Y Coordinate"),
+  startingOrientation: yup.string().oneOf(cardinalDirections).notRequired(),
+  instructions: yup
+    .string()
+    .max(100, "You have exceeded the limit for instructions")
+    .required("Please enter a step of instructions"),
+});
