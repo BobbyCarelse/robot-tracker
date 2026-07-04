2fec9cbf2344868e0add41c4c3976450c2dc822b
Bobby Carelse <bobby@juta.co.za>
2026-07-04 16:28:43 +0200

    feat: create utility to calculate current position for robot

---
+++ src/utils/DirectionUtil.ts
@@ -0,0 +1,76 @@
+import { type CardinalDirection, directions } from "../constants/app";
+
+export const isStepValidate = (step: string) => {
+  let matches = false;
+
+  directions.forEach((direction) => {
+    if (matches === true) return;
+    if (direction === step) matches = true;
+  });
+
+  return matches;
+};
+
+export const calculateCoordinates = (
+  x: number,
+  y: number,
+  direction: CardinalDirection,
+  instruction: string,
+) => {
+  let currentStep = direction;
+  let currentX = x;
+  let currentY = y;
+
+  let robotFellOff = false;
+
+  for (const character of instruction) {
+    if (!isStepValidate(character))
+      throw new Error("Invalid instructions provided");
+    if (currentY > 25 || currentY < -25) {
+      robotFellOff = true;
+    }
+    if (currentX > 25 || currentX < -25) {
+      robotFellOff = true;
+    }
+
+    if (robotFellOff) {
+      return { robotFellOff, x: currentX, y: currentY, currentStep };
+    }
+
+    if (currentStep === "E") {
+      if (character === "F") {
+        currentX = currentX + 1;
+      } else if (character === "L") {
+        currentStep = "N";
+      } else if (character === "R") {
+        currentStep = "S";
+      }
+    } else if (currentStep === "N") {
+      if (character === "F") {
+        currentY = currentY + 1;
+      } else if (character === "R") {
+        currentStep = "E";
+      } else if (character === "L") {
+        currentStep = "W";
+      }
+    } else if (currentStep === "W") {
+      if (character === "F") {
+        currentX = currentX - 1;
+      } else if (character === "R") {
+        currentStep = "N";
+      } else if (character === "L") {
+        currentStep = "S";
+      }
+    } else if (currentStep === "S") {
+      if (character === "F") {
+        currentY = currentY - 1;
+      } else if (character === "R") {
+        currentStep = "W";
+      } else if (character === "L") {
+        currentStep = "E";
+      }
+    }
+  }
+
+  return { robotFellOff, x: currentX, y: currentY, currentStep };
+};
