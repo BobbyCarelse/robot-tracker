93d50f135efdea830f71d0bdd4db1346e8f36aae
Bobby Carelse <bobby@juta.co.za>
2026-07-07 14:58:18 +0200

    tests: add test case to test scent logic

--- src/utils/DirectionUtil.test.ts
+++ src/utils/DirectionUtil.test.ts
@@ -22,9 +22,21 @@ describe('DirectionUtil', () => {
 
     describe('calculateCoordinates', () => {
         it('should calculate and return robots coords', () => {
+            const response = calculateCoordinates(1, 1, 'E', 'RFRFRFRF', 5, 3)
+
+            expect(response).toStrictEqual({robotFellOff: false, x: 1, y: 1, currentStep: 'E'})
+        })
+
+        it('should calculate and pick up that robot has fell off', () => {
             const response = calculateCoordinates(3, 2, 'N', 'FRRFLLFFRRFLL', 5, 3)
 
-            expect(response).toStrictEqual({robotFellOff: false, x: 3, y: 3, currentStep: 'N'})
+            expect(response).toStrictEqual({robotFellOff: true, x: 3, y: 3, currentStep: 'N'})
+        })
+
+        it('should ignore a move off the grid at a point that already has a scent', () => {
+            const response = calculateCoordinates(0, 3, 'W', 'LLFFFLFLFL', 5, 3, [{ x: 3, y: 3 }])
+
+            expect(response).toStrictEqual({robotFellOff: false, x: 2, y: 3, currentStep: 'S'})
         })
     })
 })
